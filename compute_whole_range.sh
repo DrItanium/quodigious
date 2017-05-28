becba5860fc1691a0ffa7f9602586e2161a9cec6
Joshua Scoggins <Theoretically.x64@gmail.com>
2017-05-28 12:24:10 -0700

    Output to the tmp directory

--- compute_whole_range.sh
+++ compute_whole_range.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "$1 2" | ./quodigious_single_top > qnums$1_2 &
-echo "$1 3" | ./quodigious_single_top > qnums$1_3 &
-echo "$1 4" | ./quodigious_single_top > qnums$1_4 &
-echo "$1 6" | ./quodigious_single_top > qnums$1_6 &
-echo "$1 7" | ./quodigious_single_top > qnums$1_7 &
-echo "$1 8" | ./quodigious_single_top > qnums$1_8 &
-echo "$1 9" | ./quodigious_single_top > qnums$1_9 &
+echo "$1 2" | ./quodigious_single_top > tmp/qnums$1_2 &
+echo "$1 3" | ./quodigious_single_top > tmp/qnums$1_3 &
+echo "$1 4" | ./quodigious_single_top > tmp/qnums$1_4 &
+echo "$1 6" | ./quodigious_single_top > tmp/qnums$1_6 &
+echo "$1 7" | ./quodigious_single_top > tmp/qnums$1_7 &
+echo "$1 8" | ./quodigious_single_top > tmp/qnums$1_8 &
+echo "$1 9" | ./quodigious_single_top > tmp/qnums$1_9 &
 wait
