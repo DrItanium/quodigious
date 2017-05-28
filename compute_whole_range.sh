ef9a3704495691b6189b6a9edfe6edbfbf140d28
Joshua Scoggins <Theoretically.x64@gmail.com>
2017-05-28 11:50:38 -0700

    Updated the whole range computer to wait on background processes

--- compute_whole_range.sh
+++ compute_whole_range.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-time echo "$1 2" | ./quodigious_single_top > qnums$1_2
-time echo "$1 3" | ./quodigious_single_top > qnums$1_3
-time echo "$1 4" | ./quodigious_single_top > qnums$1_4
-time echo "$1 6" | ./quodigious_single_top > qnums$1_6
-time echo "$1 7" | ./quodigious_single_top > qnums$1_7
-time echo "$1 8" | ./quodigious_single_top > qnums$1_8
-time echo "$1 9" | ./quodigious_single_top > qnums$1_9
+echo "$1 2" | ./quodigious_single_top > qnums$1_2 &
+echo "$1 3" | ./quodigious_single_top > qnums$1_3 &
+echo "$1 4" | ./quodigious_single_top > qnums$1_4 &
+echo "$1 6" | ./quodigious_single_top > qnums$1_6 &
+echo "$1 7" | ./quodigious_single_top > qnums$1_7 &
+echo "$1 8" | ./quodigious_single_top > qnums$1_8 &
+echo "$1 9" | ./quodigious_single_top > qnums$1_9 &
+wait
