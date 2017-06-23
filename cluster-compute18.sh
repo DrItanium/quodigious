d87a0d2aea680993136b2cc0a1f31e3c75a70115
Joshua Scoggins <Theoretically.x64@gmail.com>
2017-06-22 21:54:11 -0700

    More bug fixes... fuck

--- cluster-compute18.sh
+++ cluster-compute18.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for ((i=$1; i<=$2; i++)); do
-    ./compute18_work_unit.sh $1 > /srv/repo/data/quodigious/n18_wu$i
+for ((i=$1; j<=$2; j++)); do
+    ./compute18_work_unit.sh $j > /srv/repo/data/quodigious/n18_wu$j
 done
