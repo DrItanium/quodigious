732072b2c83fd37f0c7f2984fef79b3460eec25d
Joshua Scoggins <Theoretically.x64@gmail.com>
2017-10-24 22:04:29 -0700

    use the binary data to cache a ton of extra data for our purposes :D

--- inverted-loops.cc
+++ inverted-loops.cc
@@ -18,6 +18,7 @@
 
 // Perform quodigious checks on numbers using tons of different C++ tricks
 #include <iostream>
+#include <fstream>
 #include <cmath>
 #include <cstdint>
 #include <sstream>
@@ -29,21 +30,13 @@
 constexpr bool checkValue(u64 sum) noexcept {
 	return (isEven(sum)) || (sum % 3 == 0);
 }
-constexpr u64 performCheck(u64 sum, u64 product, u64 value) noexcept {
-	if (checkValue(sum) && isQuodigious(value, sum, product)) {
-		return value;
-	}
-	return 0;
-}
 void innerMostBody(std::ostream& stream, u64 sum, u64 product, u64 value) noexcept {
-	// inject the least significant digits 2,4,6,8
-	merge(performCheck(sum + 2, product << 1, value + 2), stream); // 2
-	merge(performCheck(sum + 4, product << 2, value + 4), stream); // 4
-	merge(performCheck(sum + 6, (product << 1) + (product << 2) , value + 6), stream); // 6
-	merge(performCheck(sum + 8, product << 3, value + 8), stream); // 8
+	if (checkValue(sum) && isQuodigious(value, sum, product)) {
+		stream << value << std::endl;
+	}
 }
 
-
+using container = std::tuple<u64, u64, u64>;
 
 template<u64 pos, u64 max>
 inline void loopBody(std::ostream& storage, u64 sum, u64 product, u64 index) noexcept;
@@ -58,49 +51,6 @@ struct ActualLoopBody {
 	ActualLoopBody(ActualLoopBody&&) = delete;
 	ActualLoopBody(const ActualLoopBody&) = delete;
 
-    template<u64 pos, u64 max>
-    static constexpr bool activateInnerThreadMode() noexcept {
-        // effective placement of the 49 threads we use will lead to effective computation!
-        // too high up and each thread does too much work, too low and the threads do too little
-        // work. The same thread placement values do not always work either!
-        //
-        // View each thread as a computation range and it should work better
-        switch(max) {
-            case 10:
-            case 11:
-                return pos == 2 || pos == 3;
-            case 12:
-                return pos == 3 || pos == 4;
-			case 13:
-				return pos == 5;
-			case 14:
-				return pos == 6;
-			case 15:
-				return pos == 8;
-			case 16:
-				return pos == 9;
-            default:
-                return false;
-        }
-    }
-    template<u64 pos, u64 max>
-    static constexpr bool activateHalvingMode() noexcept {
-        // instead of dividing by seven, do it in half
-        switch(max) {
-            case 12:
-                return pos == 2;
-			case 13:
-				return pos == 3 || pos == 4;
-            case 14:
-                return pos > 2 && pos <= 5;
-			case 15:
-                return pos > 2 && pos <= 6;
-			case 16:
-				return pos > 3 && pos <= 7;
-            default:
-                return false;
-        }
-    }
 	template<u64 pos, u64 max>
 	static void body(std::ostream& storage, u64 sum, u64 product, u64 index) noexcept {
         static constexpr auto next = fastPow10<pos - 1>;
@@ -109,85 +59,32 @@ struct ActualLoopBody {
         auto originalProduct = product;
         product <<= 1;
         sum += 2;
-        index += doubleNext;
-        if (activateInnerThreadMode<pos, max>()) {
-            auto fn = [&sum, &product, &index]() { return std::async(std::launch::async, loopBodyString<follow, max>, sum, product, index); };
-            auto p0 = fn();
-            product += originalProduct;
-            ++sum;
-            index += next;
-            auto p1 = fn();
-            product += originalProduct;
-            ++sum;
-            index += next;
-            auto p2 = fn();
-            product += (originalProduct << 1);
-            sum += 2;
-            index += doubleNext;
-            auto p3 = fn();
-            product += originalProduct;
-            ++sum;
-            index += next;
-            auto p4 = fn();
-            product += originalProduct;
-            ++sum;
-            index += next;
-            auto p5 = fn();
-            product += originalProduct;
-            ++sum;
-            index += next;
-            auto p6 = fn();
-
-            storage << p0.get() << p1.get() << p2.get() << p3.get() << p4.get() << p5.get() << p6.get();
-        } else if (activateHalvingMode<pos, max>()) {
-            auto doThree = [originalProduct](auto s, auto p, auto i) {
-                std::ostringstream output;
-                auto sum = s;
-                auto product = p;
-                auto index = i;
-				loopBody<follow, max>(output, sum, product, index); // 2
-				loopBody<follow, max>(output, sum + 1, product + originalProduct, index + next); // 3
-				loopBody<follow, max>(output, sum + 2, product + (originalProduct << 1), index + (next << 1)); // 4
-                return output.str();
-            };
-            auto p0 = std::async(std::launch::async, doThree, sum, product, index);
-			product += (originalProduct << 2);
-			sum += 4;
-            index += (doubleNext << 1);
-            auto p1 = std::async(std::launch::async, doThree, sum, product, index);
-			product += ((originalProduct << 1) + originalProduct);
-			sum += 3;
-			index += (doubleNext + next);
-            loopBody<follow, max>(storage,sum,product,index);
-
-            storage << p0.get() << p1.get();
-        } else {
-            loopBody<follow, max>(storage, sum, product, index); // 2
-            product += originalProduct;
-            ++sum;
-            index += next;
-            loopBody<follow, max>(storage, sum, product, index); // 3
-            product += originalProduct;
-            ++sum;
-            index += next;
-            loopBody<follow, max>(storage, sum, product, index); // 4
-            product += (originalProduct << 1);
-            sum += 2;
-            index += doubleNext;
-            loopBody<follow, max>(storage, sum, product, index); // 6
-            product += originalProduct;
-            ++sum;
-            index += next;
-            loopBody<follow, max>(storage, sum, product, index); // 7
-            product += originalProduct;
-            ++sum;
-            index += next;
-            loopBody<follow, max>(storage, sum, product, index); // 8
-            product += originalProduct;
-            ++sum;
-            index += next;
-            loopBody<follow, max>(storage, sum, product, index); // 9
-        }
+		index += doubleNext;
+		loopBody<follow, max>(storage, sum, product, index); // 2
+		product += originalProduct;
+		++sum;
+		index += next;
+		loopBody<follow, max>(storage, sum, product, index); // 3
+		product += originalProduct;
+		++sum;
+		index += next;
+		loopBody<follow, max>(storage, sum, product, index); // 4
+		product += (originalProduct << 1);
+		sum += 2;
+		index += doubleNext;
+		loopBody<follow, max>(storage, sum, product, index); // 6
+		product += originalProduct;
+		++sum;
+		index += next;
+		loopBody<follow, max>(storage, sum, product, index); // 7
+		product += originalProduct;
+		++sum;
+		index += next;
+		loopBody<follow, max>(storage, sum, product, index); // 8
+		product += originalProduct;
+		++sum;
+		index += next;
+		loopBody<follow, max>(storage, sum, product, index); // 9
     }
 };
 
@@ -249,13 +146,45 @@ inline std::string loopBodyString(u64 sum, u64 product, u64 index) noexcept {
 	loopBody<pos, max> (storage, sum, product, index);
 	return storage.str();
 }
+container dataCache[2097152];
 template<u64 length>
 inline void body(std::ostream& storage) noexcept {
     static_assert(length <= 19, "Can't have numbers over 19 digits at this time!");
-    loopBody<2, length>(storage, 0, 1, 0);
+	for (int i = 0; i < 2097152; ++i) {
+		auto result = dataCache[i];
+		loopBody<8, length>(storage, std::get<1>(result), std::get<2>(result), std::get<0>(result));
+	}
 }
 
 int main() {
+	std::ifstream cachedCopy("nums7", std::ifstream::in | std::ifstream::binary);
+	if (!cachedCopy.good()) {
+		std::cerr << "ERROR Couldn't open nums7 data cache! Make sure it exists and is named nums7" << std::endl;
+		return 1;
+	}
+	for (int i = 0; i < 2097152 || cachedCopy.good(); ++i) {
+		// layout is value, sum, product
+		u64 value = cachedCopy.get();
+		value |= (static_cast<u64>(cachedCopy.get()) << 8);
+		value |= (static_cast<u64>(cachedCopy.get()) << 16);
+		value |= (static_cast<u64>(cachedCopy.get()) << 24);
+		u64 sum = cachedCopy.get();
+		sum |= (static_cast<u64>(cachedCopy.get()) << 8);
+		sum |= (static_cast<u64>(cachedCopy.get()) << 16);
+		sum |= (static_cast<u64>(cachedCopy.get()) << 24);
+		u64 product = cachedCopy.get();
+		product |= (static_cast<u64>(cachedCopy.get()) << 8);
+		product |= (static_cast<u64>(cachedCopy.get()) << 16);
+		product |= (static_cast<u64>(cachedCopy.get()) << 24);
+		dataCache[i] = std::make_tuple(value, sum, product);
+	}
+	if (!cachedCopy.eof()) {
+		std::cerr << "data cache is too small!" << std::endl;
+		return 1;
+	}
+
+	cachedCopy.close();
+	
     std::ostringstream storage;
     while(std::cin.good()) {
         u64 currentIndex = 0;
