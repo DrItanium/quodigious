2788e3674dcd6bf65edf691033afac03b70f5d73
Joshua Scoggins <Theoretically.x64@gmail.com>
2017-11-24 17:01:37 -0800

    More changes

--- loops64.cc
+++ loops64.cc
@@ -189,7 +189,8 @@ inline void innerBody<0>(std::ostream& stream, u64 sum, u64 product, u64 index, 
 	}
 }
 //#include "Specialization4Digits.cc"
-#include "Specialization6Digits.cc"
+#include "Specialization5Digits.cc"
+//#include "Specialization6Digits.cc"
 //#include "Specialization8Digits.cc"
 
 template<u64 index>
