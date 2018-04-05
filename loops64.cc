e10d2c2e463e553a63b06a1c7fc09dc5b6f29fdd
Josh Scoggins <jscoggins@parasoft.com>
2018-04-05 08:15:06 -0700

    No need for strange indirection hacks anymore

--- loops64.cc
+++ loops64.cc
@@ -76,97 +76,85 @@ constexpr bool approximationCheckFailed(u64 sum) noexcept {
 }
 
 template<u64 length>
-inline void innerBody(std::ostream& stream, u64 sum, u64 product, u64 index) noexcept;
-template<u64 inner, u64 next, auto p0, auto p1, auto p2>
-std::string doHalf(u64 sum, u64 product, u64 index) noexcept {
-    std::ostringstream stream;
-    innerBody<inner>(stream, sum + p0, product * p0, index + (p0 * next));
-    innerBody<inner>(stream, sum + p1, product * p1, index + (p1 * next));
-    innerBody<inner>(stream, sum + p2, product * p2, index + (p2 * next));
-    return stream.str();
-}
-template<u64 length>
 inline void body(std::ostream& stream, u64 sum = 0, u64 product = 1, u64 index = 0) noexcept {
 	static_assert(length <= 19, "Can't have numbers over 19 digits on 64-bit numbers!");
-	static_assert(length != 0, "Can't have length of zero!");
-	// start at the most significant digit and move inward, that way we don't need
-	// to keep multiple template parameters around.
-	// a 19 digit number means multiplying by 10^18 etc
-	constexpr auto inner = length - 1;
-	// use the compiler to compute the 10^inner at compile time to eliminate further
-	// multiply operations
-	constexpr auto next = fastPow10<inner>;
-	// the current product that we get in is what we add to the running product total
-	// so if product is 4 on function call then baseProduct will be 4.
-	// Then we add the baseProduct to the running total (because this is what multiplication
-	// is).
-	//
-	// So using 4 as our base, 4 * 2 is the same as 4 + 4, 4 *3 (+ 4 4 4), etc
-	// Thus we can keep updating the product after each computation.
-	//
-	// I got the idea from strength reduction in compiler optimization theory
-	// we don't include the digits zero or 1 so just skip them by adding two
-	// or the equivalent for the corresponding thing
-	if constexpr (length >= 11) {
-		// when we are greater than 10 digit numbers, it is a smart idea to
-		// perform divide and conquer at each level above 10 digits. The number of
-		// threads used for computation is equal to: 2^(width - 10).
-        
-		auto lowerHalf = std::async(std::launch::async, doHalf<inner, next, 3, 4, 6>, sum, product, index);
-		auto upperHalf = std::async(std::launch::async, doHalf<inner, next, 7, 8, 9>, sum, product, index);
-		// perform computation on this primary thread because we want to be able
-		// to maximize how much work we do and make the amount of work in each
-		// thread even. The same number of threads are spawned but the primary
-		// thread that spawned the children is reused below.
-		innerBody<inner>(stream, sum + 2, product * 2, index + (2 * next)); // 2
-		stream << lowerHalf.get() << upperHalf.get();
-	} else {
-		// we use the stack to keep track of sums, products, and current indexes
-		// instead of starting with a whole number and breaking it apart.
-		// Walking down the call graph will cause another sum, product, and index
-		// part to be added to the provided sum, product, and index.
-		//
-		// through observational evidence, I found that the least significant
-		// digits are all even. Even if it turns out that this isn't the case
-		// I can always perform the odd digit checks later on at a significant
-		// reduction in speed cost!
-        static constexpr auto incr = (length == 1) ? 2 : 1 ;
-        for (auto i = 2; i < 10; i += incr) {
-            if ( i != 5) {
-                innerBody<inner>(stream, sum + i, product * i, index + (i * next));
+    if constexpr (length == 0) {
+        // specialization
+        bool outputToStream = false;
+        if constexpr (!expensiveChecks) {
+            if (approximationCheckFailed(sum)) {
+                // if we want to terminate early then this makes sense
+                return;
             }
         }
-	}
-}
-
-
-template<u64 length>
-inline void innerBody(std::ostream& stream, u64 sum, u64 product, u64 index) noexcept {
-	// this double template instantiation is done to make sure that the compiler
-	// does not attempt to instantiate infinitely, if this code was in place
-	// of the call to innerbody in body then the compiler would not stop
-	// instiantiating. we can then also perform specialization on length zero
-	body<length>(stream, sum, product, index);
-}
-template<>
-inline void innerBody<0>(std::ostream& stream, u64 sum, u64 product, u64 index) noexcept {
-	// specialization
-    bool outputToStream = false;
-    if constexpr (!expensiveChecks) {
-        if (approximationCheckFailed(sum)) {
-            // if we want to terminate early then this makes sense
-            return;
+        if constexpr (exact) {
+            outputToStream = ((index % product == 0) && (index % sum == 0));
+        } else {
+            outputToStream = (index % product == 0);
         }
-    }
-    if constexpr (exact) {
-        outputToStream = ((index % product == 0) && (index % sum == 0));
+        if (outputToStream) {
+            stream << index << '\n';
+        }
     } else {
-        outputToStream = (index % product == 0);
-    }
-    if (outputToStream) {
-        stream << index << '\n';
+	    //static_assert(length != 0, "Can't have length of zero!");
+	    // start at the most significant digit and move inward, that way we don't need
+	    // to keep multiple template parameters around.
+	    // a 19 digit number means multiplying by 10^18 etc
+	    constexpr auto inner = length - 1;
+	    // use the compiler to compute the 10^inner at compile time to eliminate further
+	    // multiply operations
+	    constexpr auto next = fastPow10<inner>;
+	    // the current product that we get in is what we add to the running product total
+	    // so if product is 4 on function call then baseProduct will be 4.
+	    // Then we add the baseProduct to the running total (because this is what multiplication
+	    // is).
+	    //
+	    // So using 4 as our base, 4 * 2 is the same as 4 + 4, 4 *3 (+ 4 4 4), etc
+	    // Thus we can keep updating the product after each computation.
+	    //
+	    // I got the idea from strength reduction in compiler optimization theory
+	    // we don't include the digits zero or 1 so just skip them by adding two
+	    // or the equivalent for the corresponding thing
+	    if constexpr (length >= 11) {
+	    	// when we are greater than 10 digit numbers, it is a smart idea to
+	    	// perform divide and conquer at each level above 10 digits. The number of
+	    	// threads used for computation is equal to: 2^(width - 10).
+            auto fn = [](auto sum, auto product, auto index, auto p0, auto p1, auto p2) {
+                std::ostringstream stream;
+                body<inner>(stream, sum + p0, product * p0, index + (p0 * next));
+                body<inner>(stream, sum + p1, product * p1, index + (p1 * next));
+                body<inner>(stream, sum + p2, product * p2, index + (p2 * next));
+                return stream.str();
+            };
+	    	auto lowerHalf = std::async(std::launch::async, fn, sum, product, index, 3, 4, 6);
+	    	auto upperHalf = std::async(std::launch::async, fn, sum, product, index, 7, 8, 9);
+	    	// perform computation on this primary thread because we want to be able
+	    	// to maximize how much work we do and make the amount of work in each
+	    	// thread even. The same number of threads are spawned but the primary
+	    	// thread that spawned the children is reused below.
+	    	body<inner>(stream, sum + 2, product * 2, index + (2 * next)); // 2
+	    	stream << lowerHalf.get() << upperHalf.get();
+	    } else {
+	    	// we use the stack to keep track of sums, products, and current indexes
+	    	// instead of starting with a whole number and breaking it apart.
+	    	// Walking down the call graph will cause another sum, product, and index
+	    	// part to be added to the provided sum, product, and index.
+	    	//
+	    	// through observational evidence, I found that the least significant
+	    	// digits are all even. Even if it turns out that this isn't the case
+	    	// I can always perform the odd digit checks later on at a significant
+	    	// reduction in speed cost!
+            static constexpr auto incr = (length == 1) ? 2 : 1 ;
+            for (auto i = 2; i < 10; i += incr) {
+                if ( i != 5) {
+                    body<inner>(stream, sum + i, product * i, index + (i * next));
+                }
+            }
+	    }
     }
 }
+
+
 
 //#include "Specialization2Digits.cc"
 //#include "Specialization3Digits.cc"
