bcad808e5fa1d308227b3c80e820db3131654a99
Josh Scoggins <jscoggins@parasoft.com>
2018-04-05 08:41:31 -0700

    Use structured bindings

--- numericReduction.cc
+++ numericReduction.cc
@@ -63,109 +63,107 @@ using Matches = std::list<std::tuple<u64, u64, u64>>;
 template<u64 length>
 void body(Matches& stream, const FrequencyTable& table, u64 index = 0) noexcept {
     static_assert(length <= maxWidth, "Provided length is too large!");
-    static_assert(length != 0, "Can't have length of zero!");
-    // start at the most significant digit and move inward, that way we don't need
-    // to keep multiple template parameters around.
-    // a 19 digit number means multiplying by 10^18 etc
-    constexpr auto inner = length - 1;
-    // use the compiler to compute the 10^inner at compile time to eliminate further
-    // multiply operations
-    constexpr auto next = fastPow10<inner>;
-    // the current product that we get in is what we add to the running product total
-    // so if product is 4 on function call then baseProduct will be 4.
-    // Then we add the baseProduct to the running total (because this is what multiplication
-    // is).
-    //
-    // So using 4 as our base, 4 * 2 is the same as 4 + 4, 4 *3 (+ 4 4 4), etc
-    // Thus we can keep updating the product after each computation.
-    //
-    // I got the idea from strength reduction in compiler optimization theory
-    // we don't include the digits zero or 1 so just skip them by adding two
-    // or the equivalent for the corresponding thing
-    // we use the stack to keep track of sums, products, and current indexes
-    // instead of starting with a whole number and breaking it apart.
-    // Walking down the call graph will cause another sum, product, and index
-    // part to be added to the provided sum, product, and index.
-    //
-    // through observational evidence, I found that the least significant
-    // digits are all even. Even if it turns out that this isn't the case
-    // I can always perform the odd digit checks later on at a significant
-    // reduction in speed cost!
-    if constexpr (length >= 11) {
-          auto fn = [&table, index](Matches& stream, auto start, auto end) noexcept {
-              for (auto i = start; i < end; ++i) {
-                  if (i != 5) {
-                      auto copy = table;
-                      copy.addToTable(i);
-                      copy.addToOrderHash<inner>(i);
-                      body<inner>(stream, copy, index + (i * next));
-                  }
-              }
-          };
-          Matches c0, c1;
-          auto middle = std::async(std::launch::async, fn, std::ref(c0), 4, 7);
-          auto upper = std::async(std::launch::async, fn,  std::ref(c1), 7, 10);
-          fn(stream, 2, 4);
-          middle.get();
-          for (auto const v : c0) {
-              stream.emplace_back(v);
-          }
-          upper.get();
-          for (auto const v : c1) {
-              stream.emplace_back(v);
-          }
-    } else {
-        constexpr auto incr = (length == 1) ? 2 : 1;
-        // see if we can't just make the compiler handle the incrementation
-        // instead of us thus, the code is cleaner too :D
-        for (auto i = 2; i < 10; i += incr) {
-            if (i != 5) {
-                auto copy = table;
-                copy.addToTable(i);
-                copy.addToOrderHash<inner>(i);
-                body<inner>(stream, copy, index + (i * next));
-            }
-        }
-    }
-}
-
-
-template<>
-void body<0>(Matches& stream, const FrequencyTable& table, u64 index) noexcept {
-    if constexpr (!expensiveChecks) {
-        auto sum = table.computeSum();
-        if (approximationCheckFailed(sum)) {
-            return;
-        }
-        auto product = table.computeProduct();
-        if constexpr (exact) {
-            if (index % product != 0) {
+    //static_assert(length != 0, "Can't have length of zero!");
+    if constexpr (length == 0) {
+        if constexpr (!expensiveChecks) {
+            auto sum = table.computeSum();
+            if (approximationCheckFailed(sum)) {
                 return;
             }
-            if (index % sum == 0) {
-                //stream.emplace_back(std::make_tuple(index, table.getUniqueId()));
+            auto product = table.computeProduct();
+            if constexpr (exact) {
+                if (index % product != 0) {
+                    return;
+                }
+                if (index % sum == 0) {
+                    //stream.emplace_back(std::make_tuple(index, table.getUniqueId()));
                     stream.emplace_back(index, table.getUniqueId(), table.getOrderHash());
+                }
+            } else {
+                if (index % product == 0) {
+                    //stream.emplace_back(std::make_tuple(index, table.getUniqueId()));
+                    stream.emplace_back(index, table.getUniqueId(), table.getOrderHash());
+                }
             }
         } else {
-            if (index % product == 0) {
-                //stream.emplace_back(std::make_tuple(index, table.getUniqueId()));
-                    stream.emplace_back(index, table.getUniqueId(), table.getOrderHash());
-            }
-        }
-    } else {
-        auto product = table.computeProduct();
-        if constexpr (exact) {
-            if (index % product == 0) {
-                auto sum = table.computeSum();
-                if (index % sum == 0) {
+            auto product = table.computeProduct();
+            if constexpr (exact) {
+                if (index % product == 0) {
+                    auto sum = table.computeSum();
+                    if (index % sum == 0) {
+                        stream.emplace_back(index, table.getUniqueId(), table.getOrderHash());
+                        //stream.emplace_back(std::make_tuple(index, table.getUniqueId()));
+                    }
+                }
+            } else {
+                if (index % product == 0) {
                     stream.emplace_back(index, table.getUniqueId(), table.getOrderHash());
                     //stream.emplace_back(std::make_tuple(index, table.getUniqueId()));
                 }
             }
+        }
+    } else {
+        // start at the most significant digit and move inward, that way we don't need
+        // to keep multiple template parameters around.
+        // a 19 digit number means multiplying by 10^18 etc
+        constexpr auto inner = length - 1;
+        // use the compiler to compute the 10^inner at compile time to eliminate further
+        // multiply operations
+        constexpr auto next = fastPow10<inner>;
+        // the current product that we get in is what we add to the running product total
+        // so if product is 4 on function call then baseProduct will be 4.
+        // Then we add the baseProduct to the running total (because this is what multiplication
+        // is).
+        //
+        // So using 4 as our base, 4 * 2 is the same as 4 + 4, 4 *3 (+ 4 4 4), etc
+        // Thus we can keep updating the product after each computation.
+        //
+        // I got the idea from strength reduction in compiler optimization theory
+        // we don't include the digits zero or 1 so just skip them by adding two
+        // or the equivalent for the corresponding thing
+        // we use the stack to keep track of sums, products, and current indexes
+        // instead of starting with a whole number and breaking it apart.
+        // Walking down the call graph will cause another sum, product, and index
+        // part to be added to the provided sum, product, and index.
+        //
+        // through observational evidence, I found that the least significant
+        // digits are all even. Even if it turns out that this isn't the case
+        // I can always perform the odd digit checks later on at a significant
+        // reduction in speed cost!
+        if constexpr (length >= 11) {
+              auto fn = [&table, index](Matches& stream, auto start, auto end) noexcept {
+                  for (auto i = start; i < end; ++i) {
+                      if (i != 5) {
+                          auto copy = table;
+                          copy.addToTable(i);
+                          copy.addToOrderHash<inner>(i);
+                          body<inner>(stream, copy, index + (i * next));
+                      }
+                  }
+              };
+              Matches c0, c1;
+              auto middle = std::async(std::launch::async, fn, std::ref(c0), 4, 7);
+              auto upper = std::async(std::launch::async, fn,  std::ref(c1), 7, 10);
+              fn(stream, 2, 4);
+              middle.get();
+              for (auto const v : c0) {
+                  stream.emplace_back(v);
+              }
+              upper.get();
+              for (auto const v : c1) {
+                  stream.emplace_back(v);
+              }
         } else {
-            if (index % product == 0) {
-                    stream.emplace_back(index, table.getUniqueId(), table.getOrderHash());
-                //stream.emplace_back(std::make_tuple(index, table.getUniqueId()));
+            constexpr auto incr = (length == 1) ? 2 : 1;
+            // see if we can't just make the compiler handle the incrementation
+            // instead of us thus, the code is cleaner too :D
+            for (auto i = 2; i < 10; i += incr) {
+                if (i != 5) {
+                    auto copy = table;
+                    copy.addToTable(i);
+                    copy.addToOrderHash<inner>(i);
+                    body<inner>(stream, copy, index + (i * next));
+                }
             }
         }
     }
@@ -179,7 +177,11 @@ inline void initialBody() noexcept {
     Matches elements;
     body<index>(elements, table);
     for (auto const v : elements) {
-        std::cout << std::dec << std::get<0>(v) << "\t(" << std::hex << std::get<1>(v) << ")\t(" << std::oct << std::get<2>(v) << " -> " << std::dec << std::get<2>(v) << " -> " << std::hex << std::get<2>(v) << ")\n";
+        auto [first, second, third] = v;
+        std::cout << std::dec << first << "\t(" << std::hex << second 
+            << ")\t(" << std::oct << third
+            << " -> " << std::dec << third
+            << " -> " << std::hex << third << ")\n";
     }
 }
 
