d2be5bee62b891454fe4180db8dfd5b8a3f10508
Josh Scoggins <jscoggins@parasoft.com>
2018-06-15 13:53:56 -0700

    Cleanup the boolean check logic
    the check for a != b || b != c yielded unnecessary extra evaluation
    (stuttering to be honest). Instead, first check if a != b. If that is
    the case then we can perform the other equality checks based off of
    that. If a == b, then we just see if b != c. If that is the case then
    just do two more of the six total combos. If b == c then only perform
    one execution. The downside of this approach is it causes a small DRY
    violation!
    
    This changes seems to provide a several second speed boost at 14 digits!

--- octalLikeEncoding.cc
+++ octalLikeEncoding.cc
@@ -106,6 +106,8 @@ void body(MatchList& list, u64 sum = 0, u64 product = 1, u64 index = 0) noexcept
                 auto abdiff = a != b;
                 for (auto c = b; c < 8ul; ++c) {
                     SKIP5s(c);
+                    auto c1 = c * p10a;
+                    auto c2 = c * p10b;
                     auto c3 = c * p10c;
                     auto cs = bs + c;
                     if (cs % 3 != 0) {
@@ -115,16 +117,18 @@ void body(MatchList& list, u64 sum = 0, u64 product = 1, u64 index = 0) noexcept
 #define bcheck(x) ((x % cp == 0) && (x % cs == 0))
 #define ibody(x,y,z) if (auto n = x + y + z ; bcheck(n)) { list.emplace_back(n); }
                     ibody(a1,b2,c3);
-                    if (abdiff || (b != c)) {
-                        auto c1 = c * p10a;
-                        auto c2 = c * p10b;
+                    if (abdiff) {
+                       ibody(b1,c2,a3);
+                       ibody(c1,a2,b3);
+                       if (b != c && a != c) {
+                           ibody(a1,c2,b3);
+                           ibody(b1,a2,c3);
+                           ibody(c1,b2,a3);
+                       }
+                    } else if (b != c) {
+                      // a == b in this case if we get here
                         ibody(b1,c2,a3);
                         ibody(c1,a2,b3);
-                        if ((a != c) && abdiff && (b != c)) {
-                            ibody(a1,c2,b3);
-                            ibody(b1,a2,c3);
-                            ibody(c1,b2,a3);
-                        }
                     }
                 }
             }
