69ca85ac9bec48db9c7f63ca64bb97d63de855fb
Josh Scoggins <jscoggins@parasoft.com>
2018-08-10 15:43:40 -0700

    Break computation apart into multiple queues

--- ComputeNineDigits.cc
+++ ComputeNineDigits.cc
@@ -20,6 +20,19 @@
 #include "qlib.h"
 #include <future>
 #include <list>
+#include <tuple>
+struct ComputationRequest {
+    ComputationRequest(u64 w, u64 n, u64 s, u64 p) : _width(w), _number(n), _sum(s), _product(p) { };
+    u64 getWidth() const noexcept { return _width; }
+    u64 getNumber() const noexcept { return _number; }
+    u64 getSum() const noexcept { return _sum; }
+    u64 getProduct() const noexcept { return _product; }
+private:
+    u64 _width;
+    u64 _number;
+    u64 _sum;
+    u64 _product;
+};
 template<u64 length>
 void body(std::list<u64>& values, u64 sum = 0, u64 product = 1, u64 index = 0) noexcept {
 	static_assert(length >= 10, "Cannot go lower than 10 digits!");
@@ -30,53 +43,6 @@ void body(std::list<u64>& values, u64 sum = 0, u64 product = 1, u64 index = 0) n
         if ((index % product == 0) && (index % sum == 0)) {
             values.emplace_back(index);
         }
-    } else if constexpr(length >= 18) {
-	    constexpr auto inner = length - 1;
-	    constexpr auto next = fastPow10<inner>;
-        auto t0 = std::async(std::launch::async, [](auto s, auto p, auto idx) {
-                    std::list<u64> _values;
-                    body<inner>(_values, s + 2, p * 2 , idx + (2 * next));
-                    return _values;
-                }, sum, product, index);
-        auto t1 = std::async(std::launch::async, [](auto s, auto p, auto idx) {
-                    std::list<u64> _values;
-                    body<inner>(_values, s + 3, p * 3 , idx + (3 * next));
-                    return _values;
-                }, sum, product, index);
-        auto t2 = std::async(std::launch::async, [](auto s, auto p, auto idx) {
-                    std::list<u64> _values;
-                    body<inner>(_values, s + 4, p * 4 , idx + (4 * next));
-                    return _values;
-                }, sum, product, index);
-        auto t3 = std::async(std::launch::async, [](auto s, auto p, auto idx) {
-                    std::list<u64> _values;
-                    body<inner>(_values, s + 6, p * 6 , idx + (6 * next));
-                    return _values;
-                }, sum, product, index);
-        auto t4 = std::async(std::launch::async, [](auto s, auto p, auto idx) {
-                    std::list<u64> _values;
-                    body<inner>(_values, s + 7, p * 7 , idx + (7 * next));
-                    return _values;
-                }, sum, product, index);
-        auto t5 = std::async(std::launch::async, [](auto s, auto p, auto idx) {
-                    std::list<u64> _values;
-                    for (auto i = 8; i < 10; ++i) {
-                        body<inner>(_values, s + i, p * i , idx + (i * next));
-                    }
-                    return _values;
-                }, sum, product, index);
-        auto v0 = t0.get();
-        values.splice(values.cbegin(), v0);
-        auto v1 = t1.get();
-        values.splice(values.cbegin(), v1);
-        auto v2 = t2.get();
-        values.splice(values.cbegin(), v2);
-        auto v3 = t3.get();
-        values.splice(values.cbegin(), v3);
-        auto v4 = t4.get();
-        values.splice(values.cbegin(), v4);
-        auto v5 = t5.get();
-        values.splice(values.cbegin(), v5);
     } else if constexpr(length >= 15) {
 	    constexpr auto inner = length - 1;
 	    constexpr auto next = fastPow10<inner>;
@@ -113,8 +79,30 @@ void body(std::list<u64>& values, u64 sum = 0, u64 product = 1, u64 index = 0) n
 	    }
     }
 }
-
-
+void performComputation(std::list<u64>& values, const std::list<ComputationRequest>& requests) {
+    for (const auto a : requests) {
+        auto currentSum = a.getSum();
+        auto currentProduct = a.getProduct();
+        auto currentIndex = a.getNumber();
+        if (auto div = currentIndex % 10; div == 3 || div == 5 || div == 7 || div == 9) {
+            continue;
+        }
+        switch(a.getWidth()) {
+            case 11: body<11>(values, currentSum, currentProduct, currentIndex); break;
+            case 12: body<12>(values, currentSum, currentProduct, currentIndex); break;
+            case 13: body<13>(values, currentSum, currentProduct, currentIndex); break;
+            case 14: body<14>(values, currentSum, currentProduct, currentIndex); break;
+            case 15: body<15>(values, currentSum, currentProduct, currentIndex); break;
+            case 16: body<16>(values, currentSum, currentProduct, currentIndex); break;
+            case 17: body<17>(values, currentSum, currentProduct, currentIndex); break;
+            case 18: body<18>(values, currentSum, currentProduct, currentIndex); break;
+            case 19: body<19>(values, currentSum, currentProduct, currentIndex); break;
+            default:
+                     std::cerr << "Illegal index " << currentIndex << std::endl;
+                     throw 1;
+        }
+    }
+}
 
 
 int main(int argc, char** argv) {
@@ -125,8 +113,10 @@ int main(int argc, char** argv) {
     std::string tmp(argv[1]);
     std::istringstream w(tmp);
     w >> currentWidth;
+    auto targetList = 0u;
+    std::list<ComputationRequest> _requests0, _requests1, _requests2, _requests3;
+    std::list<u64> values;
 	while(std::cin.good()) {
-        std::list<u64> values;
 		u64 currentIndex = 0;
 		u64 currentSum = 0;
 		u64 currentProduct = 0;
@@ -135,28 +125,32 @@ int main(int argc, char** argv) {
 		std::cin >> currentProduct;
         if (!std::cin.good()) { break; }
 		std::cin >> currentIndex;
-		if (std::cin.good()) {
-            if (auto div = currentIndex % 10; div == 3 || div == 5 || div == 7 || div == 9) {
-                continue;
-            }
-			switch(currentWidth) {
-				case 11: body<11>(values, currentSum, currentProduct, currentIndex); break;
-				case 12: body<12>(values, currentSum, currentProduct, currentIndex); break;
-				case 13: body<13>(values, currentSum, currentProduct, currentIndex); break;
-				case 14: body<14>(values, currentSum, currentProduct, currentIndex); break;
-				case 15: body<15>(values, currentSum, currentProduct, currentIndex); break;
-				case 16: body<16>(values, currentSum, currentProduct, currentIndex); break;
-				case 17: body<17>(values, currentSum, currentProduct, currentIndex); break;
-				case 18: body<18>(values, currentSum, currentProduct, currentIndex); break;
-				case 19: body<19>(values, currentSum, currentProduct, currentIndex); break;
-				default:
-						 std::cerr << "Illegal index " << currentIndex << std::endl;
-						 return 1;
-			}
-            for (auto v : values) {
-                std::cout << v << std::endl;
-            }
-		}
+		if (!std::cin.good()) { break; }
+        switch(targetList % 4) {
+            case 0: _requests0.emplace_back(currentWidth, currentIndex, currentSum, currentProduct); break;
+            case 1: _requests1.emplace_back(currentWidth, currentIndex, currentSum, currentProduct); break;
+            case 2: _requests2.emplace_back(currentWidth, currentIndex, currentSum, currentProduct); break;
+            case 3: _requests3.emplace_back(currentWidth, currentIndex, currentSum, currentProduct); break;
+            default:
+                    throw "Illegal index!";
+        }
+        ++targetList;
 	}
+    auto t0 = std::async(std::launch::async, [&_requests0]() { std::list<u64> values; performComputation(values, _requests0); return values; });
+    auto t1 = std::async(std::launch::async, [&_requests1]() { std::list<u64> values; performComputation(values, _requests1); return values; });
+    auto t2 = std::async(std::launch::async, [&_requests2]() { std::list<u64> values; performComputation(values, _requests2); return values; });
+    auto t3 = std::async(std::launch::async, [&_requests3]() { std::list<u64> values; performComputation(values, _requests3); return values; });
+    auto v0 = t0.get();
+    values.splice(values.cbegin(), v0);
+    auto v1 = t1.get();
+    values.splice(values.cbegin(), v1);
+    auto v2 = t2.get();
+    values.splice(values.cbegin(), v2);
+    auto v3 = t3.get();
+    values.splice(values.cbegin(), v3);
+    values.sort();
+    for (auto v : values) {
+        std::cout << v << std::endl;
+    }
 	return 0;
 }
